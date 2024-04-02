1f8062cba5b40704624d1f3853363c55817b2811
mtshwari <tphukubje7@gmail.com>
2024-04-02 12:12:55 -0700

    Python classes

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes a new square.
+
+
---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+
