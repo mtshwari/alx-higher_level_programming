dbaaaad90615ab2c3d05be6da959b5ce5eb60829
mtshwari <tphukubje7@gmail.com>
2024-06-18 09:51:08 -0700

    Javascript WarmUp

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const c = 'C is fun';
+const python = 'Python is cool';
+const javascript = 'JavaScript is amazing';
+console.log(c);
+console.log(python);
+console.log(javascript);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// script that prints 3 lines: (like 1-multi_languages.js)
+// but by using an array of string and a loop
+
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+let i = 0;
+
+while (i < array.length) {
+  console.log(array[i]);
+  i++;
+}
